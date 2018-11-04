c7621798196950dcc762d77e97ebff1644f33f7b
Jeremy Smitherman <jeremysmitherman@gmail.com>
2018-11-03 21:15:22 -0500

    Added some explosion effects and ability to kill enemies

--- Man/bullet.gd
+++ Man/bullet.gd
@@ -1,14 +1,18 @@
-extends KinematicBody2D
+extends Area2D
 
 var direction = 1
-var speed = 300
+var damage = 5
+var speed = 30000
 var velocity = Vector2()
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	connect('body_entered', self, '_on_body_entered')
 
 func _physics_process(delta):
 	velocity.x = speed * direction * delta
-	move_and_collide(velocity)
+	position += velocity * delta
+	
+func _on_body_entered(body):
+	if body.is_in_group('enemies'):
+		body.take_hit(self)
+	queue_free()
